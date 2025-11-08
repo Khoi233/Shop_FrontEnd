06fc2023b7354c406ba9bf87ec1aed53fb71b473
NT777 <xnt777x@gmail.com>
2025-11-08 08:22:41 +0700

    Added list of products

---
+++ js/products.js
@@ -0,0 +1,91 @@
+let allProducts = []
+
+// const loadProducts = async () => {
+//     try {
+//         const response = await fetch('/list_products');
+//         if (!response.ok) throw Error('Failed to fetch products.');
+
+//         const products = await response.json();
+//         allProducts = products;
+//         renderProducts(products);
+//     }
+//     catch (err) {
+//         console.error('Error loading products: ', err);
+//         document.getElementById('product-list').innerHTML = '<p>Failed to load products.</p>';
+//     }
+// };
+
+const mockProducts = [
+    {
+        name: "Laptop Pro 15",
+        id: 12123,
+        stock: 5,
+    },
+    {
+        name: "Wireless Mouse",
+        id: 11111,
+        stock: 54,
+    },
+    {
+        name: "Mechanical Keyboard",
+        id: 77777,
+        stock: 52,
+    }
+];
+
+// Simulate fetching data (mock)
+const loadProducts = async () => {
+    // Simulate delay as if fetching from server
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    allProducts = mockProducts;
+    renderProducts(allProducts);
+};
+
+const renderProducts = (products) => {
+    console.log("Rendering...");
+    const container = document.getElementById('product-list');
+    container.innerHTML = '';
+
+    if (products.length === 0) {
+        container.innerHTML = '<p style="text-align:center;">No products found.</p>';
+        return;
+    }
+
+    products.forEach((product) => {
+        const card = document.createElement('div');
+        card.className = 'product-card';
+
+        card.innerHTML = `
+        <div className="product-details">
+            <div class="product-id">ID: ${product.id}</div>
+            <div class="product-name">Name: ${product.name}</div>
+            <div class="product-stock">Remaining stock: ${product.stock}</div>
+        </div>
+        `;
+
+        container.appendChild(card);
+    })
+}
+
+const filterProducts = () => {
+    const searchInput = document.querySelector('.product-search').value.trim().toLowerCase();
+
+    if (!searchInput) {
+        renderProducts(allProducts);
+        return;
+    }
+
+    const filteredProducts = allProducts.filter(p => 
+        p.name.toLowerCase().includes(searchInput)
+    );
+
+    renderProducts(filteredProducts);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadProducts();
+
+    const searchBtn = document.getElementById('search-product-btn');
+    searchBtn.addEventListener('click', filterProducts);
+})
